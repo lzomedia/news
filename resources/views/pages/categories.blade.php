01324a196c859261eb90a1460cd13e57887873b3
Stefan <bogdan.izdrail@gmail.com>
2022-07-23 10:11:46 +0100

    Quickfix for categories page

--- resources/views/pages/categories.blade.php
+++ resources/views/pages/categories.blade.php
@@ -6,19 +6,22 @@
                 <?php
                 $article = App\Models\Article::orderBy('id', 'desc')->where('category_id', $category->id)->first();
                 ?>
-                <div class="col-sm-6 col-lg-4 mb-4">
-                    <div class="card bg-primary text-white text-center mb-3">
-                        <img alt="{{ $article->title }}" src="{{ $article->image }}" class="bd-placeholder-img card-img-top" width="100%" height="200">
-                        <div class="card-body">
-                            <h5 class="card-title">
-                                {{ $category->name }}
-                            </h5>
-                        <p class="card-text">
-                            {{ Str::words($article->summary, 5) }}
-                        </p>
+                @if($article !==null)
+                    <div class="col-sm-6 col-lg-4 mb-4">
+                        <div class="card bg-primary text-white text-center mb-3">
+                            <img alt="{{ $article->title }}" src="{{ $article->image }}"
+                                 class="bd-placeholder-img card-img-top" width="100%" height="200">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    {{ $category->name }}
+                                </h5>
+                                <p class="card-text">
+                                    {{ Str::words($article->summary, 5) }}
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                </div>
+                @endif
             @endforeach
         </div>
     </div>
